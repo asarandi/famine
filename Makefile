a3aa74c5f9ea5610be2190ed671a15c797ddde8d
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-06-23 02:36:08 -0700

    reading directory entries using a syscall

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 famine: famine.o
-	ld famine.o -o famine
+	ld -N -s famine.o -o famine
 
 famine.o:
 	nasm -f elf64 famine.s
--- famine.s
+++ famine.s
@@ -1,6 +1,10 @@
 %include "unistd_64.inc"
+%include "fcntl.inc"
 
+bits 64
+default rel
 global _start
+section .text
 
 strlen:     xor     rax, rax           
 .loop:      cmp     byte [rax + rdi], 0
@@ -22,8 +26,26 @@ _start:
             lea     rdi, [rel msg]
             call    puts
 
+            lea     rdi, [rel slash_tmp]
+            mov     rsi, O_RDONLY | O_DIRECTORY
+            mov     rax, __NR_open
+            syscall
+
+            mov     r15, rax
+            mov     rdi, rax
+            lea     rsi, [rel ptr]
+            mov     rdx, 32768
+            mov     rax, __NR_getdents64
+            syscall
+
+            mov     rdi, r15
+            mov     rax, __NR_close
+            syscall
+
+
             xor     rdi, rdi
             mov     rax, __NR_exit
             syscall
-
 msg         db "hello world",33,10,0
+slash_tmp   db ".",0
+ptr         times 0x1000 db 0
